0142168a090c3eb4d3234699375ee3ab942aafc3
Pavan Kumar <cpavankumar354@gmail.com>
2025-04-20 13:38:56 +0530

    Initial commit of backend code

--- models/user/farmdeta.js
+++ models/user/farmdeta.js
@@ -1,31 +1,4 @@
 
-// // const mongoose = require('mongoose');
-
-
-// // const UserModel = mongoose.model('user', {
-// //     title: String,
-// //     description: String,
-// // });
-
-// // module.exports = UserModel
-
-
-// const mongoose = require("mongoose");
-
-// const UserSchema = new mongoose.Schema({
-//     firstName: String,
-//     lastName: String,
-//     phone: String,
-//     zip: String,
-//     email: String,
-//     password: String
-    
-// });
-
-// const UserModule = mongoose.model("usersData", UserSchema);
-
-// module.exports = UserModule;
-
 
 
 const mongoose = require("mongoose");
